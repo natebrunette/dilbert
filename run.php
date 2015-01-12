d09d55ffe774f18fdac3fb708a6c5545250cdf54
Nate Brunette <n@tebru.net>
2015-01-12 11:34:52 -0600

    Fixing issue with file paths
    Signed-off-by: Nate Brunette <n@tebru.net>

--- run.php
+++ run.php
@@ -8,18 +8,18 @@ use Symfony\Component\Debug\ErrorHandler;
 use Tebru\DilbertPics\Application;
 
 const LOG_NAME = 'app';
-const LOG_LOCATION = 'var/log/app.log';
+const LOG_LOCATION = '/var/log/app.log';
 const LOG_EMAIL_TO = 'n+dilberterror@tebru.net';
 const LOG_MESSAGE = 'An error occurred';
 const LOG_EMAIL_FROM = 'system@dilbertpics.com';
 
 require 'vendor/autoload.php';
 
-AnnotationRegistry::registerAutoloadNamespace('JMS\Serializer\Annotation', 'vendor/jms/serializer/src');
+AnnotationRegistry::registerAutoloadNamespace('JMS\Serializer\Annotation', __DIR__ . '/vendor/jms/serializer/src');
 
 
 $logger = new Logger(LOG_NAME);
-$logger->pushHandler(new StreamHandler(LOG_LOCATION));
+$logger->pushHandler(new StreamHandler(__DIR__ . LOG_LOCATION));
 $logger->pushHandler(new NativeMailerHandler(LOG_EMAIL_TO, LOG_MESSAGE, LOG_EMAIL_FROM));
 
 $errorHandler = ErrorHandler::register();
